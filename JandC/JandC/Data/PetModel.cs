93f0a7b6c25e60ee2d1bd665b1fa5bd468fb2572
桂素伟 <axzxs2001@163.com>
2019-04-25 12:30:00 +0900

    自定义模板，字符编码坑很多呀！

---
+++ JandC/JandC/Data/PetModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JandC.Data
+{
+    public class PetModel
+    {
+        public int PetId { get; set; }
+        public string Name { get; set; }
+    }
+}
